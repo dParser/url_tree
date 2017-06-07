83cddca95e2294ecd19d5e32844bc947297556e1
dParser <d@tarifer.ru>
2017-06-07 11:42:48 +0100

    Добавлен метод first_url_add - Добавить элемент в список URL 1-ого уровня

--- url_tree.rb
+++ url_tree.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 class UrlTree
 
+	first_url_list = ""
+
   # Метод "Вывод на экран списка URL 1-ого уровня":
   def first_url_put
     puts "Когда-нибудь вместо этих слов мы увидим список URL 1-ого уровня!"
@@ -11,6 +13,10 @@ class UrlTree
     puts "Когда-нибудь вместо этого вывода мы создадим список URL 1-ого уровня!"
   end
 
+  # Метод "Добавить элемент в список URL 1-ого уровня":
+  def first_url_add
+    puts "Список URL 1-ого уровня: #first_url_list!"
+  end
 end
   
 urls=UrlTree.new
