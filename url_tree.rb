7312d92092ba88d28faaca269740f865581eb817
dParser <d@tarifer.ru>
2017-06-07 12:19:58 +0300

    Добавлен файл url_tree.rb

---
+++ url_tree.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+class UrlTree
+
+  # Метод "Вывод на экран списка URL 1-ого уровня":
+  def first_url_put
+    puts "Когда-нибудь вместо этих слов мы увидим список URL 1-ого уровня!"
+  end
+
+  # Метод "Создания списка URL 1-ого уровня":
+  def first_url_create
+    puts "Когда-нибудь вместо этого вывода мы создадим список URL 1-ого уровня!"
+  end
+
+end
+  
+urls=UrlTree.new
+urls.first_url_put  
+urls.first_url_create
