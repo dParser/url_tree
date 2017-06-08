812130c7598487827a106decd9db7f56bdd79bbd
dParser <d@tarifer.ru>
2017-06-08 09:48:05 +0100

    Доработаны методы. Добалено меню выбора для пользователя. Добавлен CASE для обработки выбора пользователя

--- url_tree.rb
+++ url_tree.rb
@@ -1,27 +1,56 @@
 # encoding: utf-8
 class UrlTree
 
-	def initialize(first_url_list = ["beeline.ru", "megafon.ru", "mts.ru"])
-		@first_url_list = first_url_list
-	end
+  def initialize(first_url_list = ["beeline.ru", "megafon.ru", "mts.ru", "tele2.ru"])
+    # Список url 1-ого уровня
+    @first_url_list = first_url_list
+  end
 
   # Метод "Вывод на экран списка URL 1-ого уровня":
   def first_url_put
-    puts "Когда-нибудь вместо этих слов мы увидим список URL 1-ого уровня!"
+    puts "\nСписок URL 1-ого уровня:"
+    puts @first_url_list
   end
 
   # Метод "Создание списка URL 1-ого уровня":
   def first_url_create
-    puts "Когда-нибудь вместо этого вывода мы создадим список URL 1-ого уровня!"
+    puts "\nКогда-нибудь вместо этого вывода мы создадим список URL 1-ого уровня!"
   end
 
   # Метод "Добавить элемент в список URL 1-ого уровня":
   def first_url_add
-    puts "Список URL 1-ого уровня: #{@first_url_list}"
+    puts "\nСписок URL 1-ого уровня:"
+    puts @first_url_list
+    puts "\nВведите доменное имя сайта нового оператора:"
+    input = gets
+    @first_url_list.push(input.chomp)
+    puts "\nОбновленный список URL 1-ого уровня:"
+    puts @first_url_list
   end
 end
-  
-urls=UrlTree.new
-urls.first_url_put  
-urls.first_url_create
-urls.first_url_add
+
+# Меню выбора действий:
+puts "\nЧто вы хотите сделать?"
+puts "Создать список URL 1-ого уровня - нажмите 1"
+puts "Добавить элемент в список URL 1-ого уровня - нажмите 2"
+puts "Вывести на экран списка URL 1-ого уровня - нажмите 3"
+puts "Выйти из программы - любую другую клавишу"
+input = gets
+input = input.chomp
+
+# Создание объекта:
+urls=UrlTree.new ["beeline.ru", "megafon.ru", "mts.ru", "tele2.ru"]
+
+# case-условие:
+case input.to_i
+   when 1
+     urls.first_url_create
+   when 2
+     urls.first_url_add
+   when 3
+     urls.first_url_put  
+   else return
+end
+
+
+
