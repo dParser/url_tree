5ff806320b4d75b7df9847b22c6deb5706316185
parser <e.pitko@tarifer.ru>
2017-06-22 13:44:16 +0400

    В структуру файлов level-1.csv и level-2.csv добавлено поле категории тарифов. В связи с этим, отключена инициализация объекта из тела программы, сделаны поправки вметодах, работающих с этими файлами, поправлены комментарии

--- url_tree.rb
+++ url_tree.rb
@@ -2,13 +2,22 @@
 
 # ОПИСАНИЕ
 # 1. В файле regions.csv хранится инф. по регионам в виде:
-#   Оператор связи(лат.);Регион(лат.)
+#     Оператор связи(лат.);
+#     Регион(лат.)
 #
 # 2. В файле level-1.csv хранится инф. по стартовым URL в виде:
-#   Оператор связи(лат.);Домен сайта;Тип клиента;Cтартовое URL в виде шаблона
+#     Оператор связи(лат.);
+#     Домен сайта;
+#     Тип клиента;
+#     Категория тарифного плана;    (для смартфона, планшета, роутера и т.д.)
+#     Cтартовое URL в виде шаблона
 #
 # 3. На основании файлов regions.csv и level-1.csv строится файл level-2.csv в виде:
-#   Оператор связи(лат.);Домен сайта;Тип клиента;URL региона
+#     Оператор связи(лат.);
+#     Домен сайта;
+#     Тип клиента;
+#     Категория тарифного плана;    (для смартфона, планшета, роутера и т.д.)
+#     URL региона
 
 $path = File.dirname(__FILE__)
 $db_path = $path + '/DB'
@@ -28,37 +37,38 @@ class FirstLevelUrls
         @first_url_hashs.push({:oss=>arr[0],   # Название ОСС (лат.)
                                :domen=>arr[1], # Доменное имя сайта ОСС
                                :b2_x=>arr[2],  # Тип клиена (корп., физ.)
-                               :starting_url=>arr[3] # Стартовое URL
+                               :category=>arr[3],  # Категория т.планов
+                               :starting_url=>arr[4]  # Стартовое URL
                              })
       }
-    # Загрузка URL 1-ого уровня из тела программы в массив хэшей:
-    else
-      @first_url_hashs = [{:oss=>"Билайн",
-                         :domen=>"beeline.ru",
-                         :b2_x=>"b2c",
-                         :starting_url=>"https://moskva.beeline.ru/customers/products/"
-                        },
-                        {:oss=>"Мегафон",
-                         :domen=>"megafon.ru",
-                         :b2_x=>"b2c",
-                         :starting_url=>"https://moscow.megafon.ru/"
-                        },
-                        {:oss=>"МТС",
-                         :domen=>"mts.ru",
-                         :b2_x=>"b2c",
-                         :starting_url=>"http://www.mts.ru/"
-                        },
-                        {:oss=>"TELE2",
-                         :domen=>"tele2.ru",
-                         :b2_x=>"b2c",
-                         :starting_url=>"https://msk.tele2.ru/?pageParams=askForRegion%3Dtrue"
-                        },
-                        {:oss=>"Мотив",
-                         :domen=>"motivtelecom.ru",
-                         :b2_x=>"b2c",
-                         :starting_url=>"http://motivtelecom.ru/ekb/"
-                        }
-                       ]
+#    # Загрузка URL 1-ого уровня из тела программы в массив хэшей:
+#    else
+#      @first_url_hashs = [{:oss=>"Билайн",
+#                         :domen=>"beeline.ru",
+#                         :b2_x=>"b2c",
+#                         :starting_url=>"https://moskva.beeline.ru/customers/products/"
+#                        },
+#                        {:oss=>"Мегафон",
+#                         :domen=>"megafon.ru",
+#                         :b2_x=>"b2c",
+#                         :starting_url=>"https://moscow.megafon.ru/"
+#                        },
+#                        {:oss=>"МТС",
+#                         :domen=>"mts.ru",
+#                         :b2_x=>"b2c",
+#                         :starting_url=>"http://www.mts.ru/"
+#                        },
+#                        {:oss=>"TELE2",
+#                         :domen=>"tele2.ru",
+#                         :b2_x=>"b2c",
+#                         :starting_url=>"https://msk.tele2.ru/?pageParams=askForRegion%3Dtrue"
+#                        },
+#                        {:oss=>"Мотив",
+#                         :domen=>"motivtelecom.ru",
+#                         :b2_x=>"b2c",
+#                         :starting_url=>"http://motivtelecom.ru/ekb/"
+#                        }
+#                       ]
     end
   end
 
@@ -88,6 +98,7 @@ class FirstLevelUrls
       puts hh[:oss]
       puts "\tДомен: #{hh[:domen]}"
       puts "\tТип клиена: #{hh[:b2_x]}"
+      puts "\tКатегория тарифов: #{hh[:category]}"
       puts "\tСтартовое URL: #{hh[:starting_url]}"
     }
   end
@@ -107,7 +118,7 @@ class FirstLevelUrls
                                  # [1] - Регион (лат.)
         if arr[0] == hh[:oss]
           tmp = hh[:starting_url].gsub("^", arr[1]) # в URL: "^" заменить на назв. региона
-          file_obj_level2.puts("#{hh[:oss]};#{hh[:domen]};#{hh[:b2_x]};#{tmp}")
+          file_obj_level2.puts("#{hh[:oss]};#{hh[:domen]};#{hh[:b2_x]};#{hh[:category]};#{tmp}")
         end
       }
     }
